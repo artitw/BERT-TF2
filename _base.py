b536d15000748134bf69fda976373c2d90906dc4
Artit 'Art' Wangperawong <artitw@gmail.com>
2019-12-21 17:56:48 -0500

    update imports

--- _base.py
+++ _base.py
@@ -21,8 +21,8 @@ from __future__ import print_function
 from absl import flags
 import tensorflow as tf
 
-from official.utils.flags._conventions import help_wrap
-from official.utils.logs import hooks_helper
+from _conventions import help_wrap
+import hooks_helper
 
 
 def define_base(data_dir=True, model_dir=True, clean=False, train_epochs=False,
